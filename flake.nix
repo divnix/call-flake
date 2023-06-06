cd983386bf43374a612b7f704379c8088a6e633d
Timothy DeHerrera <tim@nrdxp.dev>
2023-06-06 11:48:34 -0600

    refactor: compute lock string separately

--- flake.nix
+++ flake.nix
@@ -3,26 +3,32 @@
     callFlake = import ./call-flake.nix;
   in {
     __functor = _:
-      # ref can either be expressed as an attribute set or a path to source tree
-      ref: let
-        # if expressed as attribute set, a dir argument may be added to point to a subref
-        dir = ref.dir or "";
-        src = if builtins.isPath ref' then { outPath = ref'; } else builtins.fetchTree ref';
-        ref' =
-         if builtins.isAttrs ref
-         then builtins.removeAttrs ref ["dir"]
-         else ref;
-        defaultLock = builtins.toJSON {
-          nodes.root = {};
-          root = "root";
-        };
-        readOrDefault = path:
-          if builtins.pathExists path
-          then builtins.readFile path
-          else defaultLock;
+    # ref can either be expressed as an attribute set or a path to source tree
+    ref: let
+      # if expressed as attribute set, a dir argument may be added to point to a subref
+      dir = ref.dir or "";
+      src =
+        if builtins.isPath ref'
+        then {outPath = ref';}
+        else builtins.fetchTree ref';
+      ref' =
+        if builtins.isAttrs ref
+        then builtins.removeAttrs ref ["dir"]
+        else ref;
+      lockstr = let
+        rootlock = "${src}/flake.lock";
+        sublock = "${src}/${dir}/flake.lock";
       in
-        if dir == ""
-        then callFlake (readOrDefault "${src}/flake.lock") src dir
-        else callFlake (readOrDefault "${src}/${dir}/flake.lock") src dir;
+        if dir == "" && builtins.pathExists rootlock
+        then builtins.readFile rootlock
+        else if dir != "" && builtins.pathExists sublock
+        then builtins.readFile sublock
+        else
+          builtins.toJSON {
+            nodes.root = {};
+            root = "root";
+          };
+    in
+      callFlake lockstr src dir;
   };
 }
