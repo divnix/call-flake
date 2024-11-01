5228787ef8cd409c6f73cefc04fa688483ea8ab4
Kamillaova <me@kamillaova.dev>
2024-11-01 21:32:19 +0300

    Add default.nix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,7 @@
 {
   outputs = inputs: let
-    callFlake = import ./call-flake.nix;
+    wrapper = import ./.;
   in {
-    __functor = _:
-    # ref can either be expressed as an attribute set or a path to source tree
-    ref: let
-      # if expressed as attribute set, a dir argument may be added to point to a subref
-      dir = ref.dir or "";
-      src =
-        if builtins.isPath ref || builtins.isString ref
-        then {outPath = ref;}
-        else builtins.fetchTree (
-          if builtins.isAttrs ref
-          then builtins.removeAttrs ref ["dir"]
-          else ref
-        );
-      lockstr = let
-        rootlock = "${src}/flake.lock";
-        sublock = "${src}/${dir}/flake.lock";
-      in
-        if dir == "" && builtins.pathExists rootlock
-        then builtins.readFile rootlock
-        else if dir != "" && builtins.pathExists sublock
-        then builtins.readFile sublock
-        else
-          builtins.toJSON {
-            nodes.root = {};
-            root = "root";
-          };
-    in
-      callFlake lockstr { root = { sourceInfo = src; subdir = dir; }; };
+    __functor = _: wrapper;
   };
 }
